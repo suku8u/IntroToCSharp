41b6183d4b47e2a2e782672851b9f8d6222ce4cd
sponram <suku8u@uw.edu>
2016-04-11 00:08:35 -0400

    CS Fundamentals First Commit

---
+++ IntroToCSharp/HelloCSharp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCSharp
+{
+    class HelloCSharp
+    {
+        static void Main(string[] args)
+        {
+            /*
+            for (int i = 1; i <= 100; i++){
+                System.Console.WriteLine(i);
+            }*/
+            //System.Console.WriteLine("Good day Sukumar Ponram !! ");
+            //System.Console.WriteLine(DateTime.Now);
+            //System.Console.WriteLine(Math.Sqrt(12345));
+        }
+    }
+}
